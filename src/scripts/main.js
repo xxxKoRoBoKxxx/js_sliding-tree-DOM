9fce6f051156fde65c4506049d1c90b54a3ff284
Andrii Filipenko <andreyfil1337@gmail.com>
2025-03-26 15:35:49 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-// write code here
+const titles = [...document.querySelectorAll('li')].filter(
+  (li) => li.childElementCount > 0,
+);
+
+function listMover(event) {
+  event.stopPropagation();
+
+  const target = event.target;
+
+  if (!(target instanceof Node)) {
+    return;
+  }
+
+  const parent = target.parentElement;
+
+  if (parent.listRolled) {
+    parent.lastElementChild.style = '';
+    parent.listRolled = false;
+  } else if (parent.listRolled === false) {
+    parent.lastElementChild.style.height = '0';
+    parent.lastElementChild.style.visibility = 'hidden';
+    parent.listRolled = true;
+  }
+}
+
+titles.forEach((li) => {
+  const span = document.createElement('span');
+  const titleContent = li.firstChild.data.trim();
+  const spanContent = document.createTextNode(titleContent);
+
+  span.appendChild(spanContent);
+  li.replaceChild(span, li.firstChild);
+  li.listRolled = false;
+
+  span.addEventListener('click', listMover);
+});
